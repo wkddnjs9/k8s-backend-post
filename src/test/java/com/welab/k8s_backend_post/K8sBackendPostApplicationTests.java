6357d6275b6940b2944cc3813de3400ec32ee87f
wkddnjs9 <78907516+wkddnjs9@users.noreply.github.com>
2025-06-16 14:17:16 +0900

    초기세팅

--- src/test/java/com/welab/k8s_backend_post/K8sBackendPostApplicationTests.java
+++ src/test/java/com/welab/k8s_backend_post/K8sBackendPostApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class K8sBackendPostApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
